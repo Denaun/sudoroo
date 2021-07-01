4c8ba406fd0ade626b7e17cfdf1c2266cd9d9293
Maurizio Zucchelli <mauzuc90@yahoo.it>
2021-07-01 08:03:52 +0200

    Update the time when the game is updated.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ApplicationRecord
   include Gridled
 
+  include ActionView::RecordIdentifier
+
   belongs_to :puzzle
   broadcasts
+  after_update_commit -> { broadcast_replace_later target: dom_id(self, :time), partial: 'games/time' }
 end
