c2bf6b39d3d1bfaffe851f26d1b9ef6143ec3139
Maurizio Zucchelli <mauzuc90@yahoo.it>
2021-07-01 13:05:49 +0200

    Detect if a game has been finished.

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,4 +6,11 @@ class Game < ApplicationRecord
   belongs_to :puzzle
   broadcasts
   after_update_commit -> { broadcast_replace_later target: dom_id(self, :time), partial: 'games/time' }
+
+  def finished?
+    return false if grid.include? ' '
+
+    SolutionExistenceValidator.new.validate self
+    errors.empty?
+  end
 end
